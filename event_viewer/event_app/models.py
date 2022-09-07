0ab7445b0d46bd121600836c90ea1022527a21a5
randall <randall.mayton@gmail.com>
2022-09-06 19:29:12 -0700

    Added to models

--- event_viewer/event_app/models.py
+++ event_viewer/event_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User (models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    user_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    event_votes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    posted_by = models.ForeignKey(User.user_name, on_delete=models.CASCADE, related_name="events")
+
+
+
+
