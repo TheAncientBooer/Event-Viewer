9c9db4c2eb8851223afa4ed517f6937d38e42a4d
randall <randall.mayton@gmail.com>
2022-09-12 18:58:04 -0700

    delete to refresh project

--- event_viewer/event_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render 
-from .models import User, Event
-from django.http import HttpResponse, HttpResponseRedirect
-
-
-
-def index(request):
-    return render(request, 'index.html')
