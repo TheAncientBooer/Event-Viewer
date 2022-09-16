9409ca388879a06648c59bd6f01c4dc38d819561
randall <randall.mayton@gmail.com>
2022-09-15 19:14:06 -0700

    Map stuff

--- map_project/map/views.py
+++ map_project/map/views.py
@@ -8,7 +8,7 @@ import geocoder
 # Create your views here.
 
 
-def index(request):
+def index(request): 
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
@@ -24,16 +24,31 @@ def index(request):
     if lat == None or lng == None:
         address.delete()
         return HttpResponse('You address input is invalid')
-
+    
     # Create Map Object
-    m = folium.Map(location=[45.52592691269178, -122.65383574156633], zoom_start=2)
+    m = folium.Map(location=[45.52592691269178, -122.65383574156633], zoom_start=2,)
 
-    folium.Marker([lat, lng], tooltip='Click for more',
-                  popup=country).add_to(m)
+    tooltip = "View Event!"
+
+    folium.Marker([45.3288, -121.6625], popup="<i>Mt. Hood Meadows</i>", tooltip=tooltip).add_to(m)
+    folium.Marker([45.3311, -121.7113], popup="<i>Timberline Lodge</i>", tooltip=tooltip).add_to(m)
+    folium.Marker([33.41063301181929, -82.13471009798371], popup="<i>Annex</i>", tooltip=tooltip).add_to(m)
+
+    # m.save("index.html")
+
+
+    # m = folium.Map(location=[45.52592691269178, -122.65383574156633], zoom_start=1.5)
+
+    # folium.Marker([lat, lng], tooltip='Click for more',
+    #               popup=country).add_to(m)
     # Get HTML Representation of Map Object
+    
+    
     m = m._repr_html_()
     context = {
         'm': m,
         'form': form,
     }
+    
     return render(request, 'index.html', context)
+    
