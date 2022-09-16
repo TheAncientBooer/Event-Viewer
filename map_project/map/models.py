3239ece94514399e527721c0a227bd5628b5aa3b
randall <randall.mayton@gmail.com>
2022-09-15 18:10:27 -0700

    models

--- map_project/map/models.py
+++ map_project/map/models.py
@@ -1,3 +1,4 @@
+import json
 from django.db import models
 
 # Create your models here.
@@ -9,3 +10,9 @@ class Search(models.Model):
 
     def __str__(self):
         return self.address
+class Event(models.Model):
+     title = models.CharField(max_length=200, null=True)
+     description = models.TextField(null=True)
+     location = models.CharField(max_length=500, null=True)
+     start_date = models.DateTimeField()
+     end_date = models.DateTimeField()
