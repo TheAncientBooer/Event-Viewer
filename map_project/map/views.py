bfa3d0c336e40bdc4c9addbf91c86f2f44d11aeb
randall <randall.mayton@gmail.com>
2022-09-15 19:45:14 -0700

    qs

--- map_project/map/views.py
+++ map_project/map/views.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Search
@@ -30,17 +31,19 @@ def index(request):
 
     tooltip = "View Event!"
 
-    folium.Marker([45.3288, -121.6625], popup="<i>Mt. Hood Meadows</i>", tooltip=tooltip).add_to(m)
+    folium.Marker([45.3288, -121.6625], popup="<i>Mt. Hood Meadows</i>", tooltip=tooltip).add_to(m) #change tooltip to event title
     folium.Marker([45.3311, -121.7113], popup="<i>Timberline Lodge</i>", tooltip=tooltip).add_to(m)
     folium.Marker([33.41063301181929, -82.13471009798371], popup="<i>Annex</i>", tooltip=tooltip).add_to(m)
 
+    #display map
+    m.save('map.html')
     # m.save("index.html")
 
 
     # m = folium.Map(location=[45.52592691269178, -122.65383574156633], zoom_start=1.5)
 
-    # folium.Marker([lat, lng], tooltip='Click for more',
-    #               popup=country).add_to(m)
+    folium.Marker([lat, lng], tooltip='Click for more',
+                   popup=country).add_to(m)
     # Get HTML Representation of Map Object
     
     
