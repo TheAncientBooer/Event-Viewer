c2491f0cc071809121b7f4a8fa5721ae0cdee8d4
randall <randall.mayton@gmail.com>
2022-09-19 22:12:25 -0700

    updated models

--- map_project/map/admin.py
+++ map_project/map/admin.py
@@ -1,7 +1,9 @@
+from multiprocessing import Event
 from django.contrib import admin
-from .models import Search
+from .models import Search, Event
 
 # Register your models here.
 admin.site.register(Search)
+admin.site.register(Event)
 
 # Register your models here.
--- map_project/map/models.py
+++ map_project/map/models.py
@@ -10,5 +10,9 @@ class Event(models.Model):
      title = models.CharField(max_length=200, null=True)
      description = models.TextField(null=True)
      location = models.CharField(max_length=500, null=True)
-     start_date = models.DateTimeField()
-     end_date = models.DateTimeField()
+     start_date = models.DateField()
+     end_date = models.DateField()
+     website = models.URLField(max_length=1000, null=True)
+
+     def __str__(self):
+            return self.title
