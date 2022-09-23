bfeb9162b548bdbd14aa1e419f6f5ace37255311
randall <randall.mayton@gmail.com>
2022-09-22 19:40:51 -0700

    qs

--- map_project/map/forms.py
+++ map_project/map/forms.py
@@ -1,18 +1,19 @@
 from django import forms
 from .models import Search, Event
+from django.forms import ModelForm
 class SearchForm(forms.ModelForm):
     address = forms.CharField(label='')
     class Meta:
         model = Search
         fields = ['address']
 
-class EventForm(forms.Form): #WAS    forms.ModelForm):
-    title = forms.CharField(label='title' , max_length=200)
-    description = forms.CharField(label='description')
-    location = forms.CharField(label='location', max_length=500)
-    start_date = forms.DateField(label='Start Date', widget=forms.SelectDateWidget)
-    end_date = forms.DateField(label='End Date', widget=forms.SelectDateWidget)
-    website = forms.URLField(label='Website', max_length=1000, required=False)
+class EventForm(ModelForm): #WAS    forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = '__all__'
+# form = EventForm()    
+# event = Event.objects.get(pk=1)
+# form = EventForm(instance=event)
 
     # class Meta:
     #     model = Event
--- map_project/map_project/urls.py
+++ map_project/map_project/urls.py
@@ -3,11 +3,12 @@ from map import views as map_views
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', map_views.index, name='index'),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('post_event/', TemplateView.as_view(template_name='post_event.html'), name='post_event'),
+    path('event/', include('map.urls')),
 ] 
