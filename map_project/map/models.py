12a3985394a7e40f4f3e1b929e5ba5d0d4026866
randall <randall.mayton@gmail.com>
2022-10-01 20:12:33 -0700

    c

--- map_project/map/models.py
+++ map_project/map/models.py
@@ -12,6 +12,9 @@ class Event(models.Model):
      event_location = models.CharField(max_length=500, null=True)
      start_date = models.DateField() 
      end_date = models.DateField()
+
+    #  def event_location(self):
+    #         return self.event_location
     
      def __str__(self):
             return self.title  
